dffb970a328345f797eb09764230b5ddd7968c80
v.semenov <v.semenov@itdept.cloud>
2025-02-03 22:32:28 +0200

    rm chats2 from config

--- bot/config.py
+++ bot/config.py
@@ -9,7 +9,6 @@ TOKEN = os.getenv('BOT_TOKEN')
 PAYMENT_WALLET = os.getenv('PAYMENT_WALLET')
 
 group_chat_ids = [int(val) for val in os.getenv('group_chat_ids').split(',')]
-group_2_chat_ids = [int(val) for val in os.getenv('group_2_chat_ids').split(',')]
 notification_chat_id = os.getenv('notification_chat_id')
 admins = [int(val) for val in os.getenv('admins').split(',')]
 
